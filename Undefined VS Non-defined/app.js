36b9a289afc08247ca9eb08dc4af33884673d7d9
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2025-12-31 00:50:00 +0530

    Undefined Vs Not-defined

---
+++ Undefined VS Non-defined/app.js
@@ -0,0 +1,23 @@
+// console.log(a);
+// var a = 7;
+var a;
+console.log(a);
+a = 10;
+console.log(a);
+a = "Hello World";
+console.log(a);
+// console.log(x);
+if(a === undefined){
+    console.log("a is undefined");
+}
+else{
+    console.log("a is not defined");
+}
+try {
+  console.log(x);
+} catch (e) {
+  console.log(e.message);
+}
+var b = undefined;
+console.log(b);
+
