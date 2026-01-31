ebd32a1aee9784f2212c6c7dd2b268d452a581f3
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-02-01 00:54:49 +0530

    Const Declarations

--- Temporal Dead Zone (Let & Const)/app.js
+++ Temporal Dead Zone (Let & Const)/app.js
@@ -4,3 +4,11 @@ let a = 10; // Temporal dead zone ends
 console.log(a);
 var b = 100;
 console.log(x);
+// let a = 100; // Uncaught SyntaxError :"Indentifier a has been already declared"
+// console.log(a);
+
+// Const Declaration !!
+// const c; Both the statements are not possible...
+// c = 1000;
+// const c = 2000; Declaration and initialization should be done in the same line...
+//c = 600; It will give the typeError....As Already assigned to const variable...
