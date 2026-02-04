61e15993217a8ee8db13c2c99b0da76a1ad0f548
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-02-05 00:05:29 +0530

    Block Scope & Shadowing

---
+++ Block Scope & Shadowing/index.js
@@ -0,0 +1,25 @@
+/*{
+    //Compound Statement
+    var a = 10;
+    console.log(a);
+} */
+// if(true)console.log("Swastik");
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+var a = 100;
+console.log(a);
+let b = 100;
+console.log(b);
+const c = 1000;
+console.log(c);
+
