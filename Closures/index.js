ac447bff172fdbb8c64091d6d36f777f0cf57744
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-02-08 03:09:39 +0530

    Understanding of Closures

--- Closures/index.js
+++ Closures/index.js
@@ -1,8 +1,36 @@
+// Example of Closures
+// function x(){
+//     var a = 7;
+//     function y (){
+//         console.log(a);
+//     }
+//     y();
+// }
+// x();
+
+// Impact of return keyword
+
 function x(){
     var a = 7;
     function y (){
         console.log(a);
     }
-    y();
+    return y;
 }
-x();
+var z = x(); // Calling a function or invokation of a function 
+console.log(z);
+// After this x is totally gone no existance of x in the call stack...
+z();
+
+// Corner Cases
+function x(){
+    var a = 7;
+    function y (){
+        console.log(a);
+    }
+    a = 100;
+    return y;
+}
+var z = x(); 
+console.log(z);
+z();
