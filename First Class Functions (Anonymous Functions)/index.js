91a5a8db8f8e9f904ad686c153161ca5995fcaf8
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-02-28 23:56:36 +0530

    First Class Functions

---
+++ First Class Functions (Anonymous Functions)/index.js
@@ -0,0 +1,21 @@
+a();
+b();
+//Function Statement aka Function Declaration
+function a() {
+    console.log("a called");
+}
+
+//Function Expression
+var b = function () {
+    console.log("b called");
+}
+
+
+//Anonymous Function
+function() {
+
+}
+//Named Function Expression
+//Difference between Parameters and Arguments
+//First Class Functions
+//Arrow Functions
