089271bf7743af21708e6c097f13b6e6a7f7e4cb
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-03-06 20:53:10 +0530

    Completed the vrious types of functions and difference between Parameters and Arguments

--- First Class Functions (Anonymous Functions)/index.js
+++ First Class Functions (Anonymous Functions)/index.js
@@ -19,10 +19,8 @@ b();
 var c = function xyz(){
     console.log("c called");
 }
-xyz();
-
-
-
+xyz(); //It will throw error as it is executed in the outerscope but it will not give any error if it is called in the local scope 
 //Difference between Parameters and Arguments
+ 
 //First Class Functions
 //Arrow Functions
