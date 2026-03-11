87256524932ed7d1d684c771f9dde6a158ca5b82
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2026-03-12 00:48:33 +0530

    Added the button

--- Callback Functions in JS/index.js
+++ Callback Functions in JS/index.js
@@ -10,3 +10,23 @@ function x(y) {
 x(function y() {
     console.log("y");
 }); //function y is the callback function
+// Creating a Event Listener
+// document.getElementById("clickMe")
+//     .addEventListener("click", function xyz() {
+//         console.log("Button Clicked");
+//     })
+//Closures along with Event Listeners
+//1st Method Using Global Variable (But Not A Good Idea)
+// let count = 0;
+// document.getElementById("clickMe").addEventListener("click", function xyz() {
+//     console.log("Button Clicked", ++count)
+// })
+//Using Closures
+function attachEventListeners(){
+    let count = 0;
+    document.getElementById("clickMe").
+    addEventListener("click", function xyz() {
+        console.log("Button Clicked", ++count);
+    });
+}
+attachEventListeners();
